75a992a70bc2bfca40c87cb4bace782b6314da9e
avintha97 <avintha.geo@gmail.com>
2023-02-09 17:32:49 +0530

    check usig mysql

--- connect.php
+++ connect.php
@@ -1,23 +1,11 @@
 <?php
 
-// $con = new mysqli("localhost","root","avintha97",'simple_crud');
+$conect = new mysqli("localhost","root","avintha97",'simple_crud');
 
-// if(!$con){
-//     die(mysqli_error($con));
-// }
+if($conect){
+    echo "conected succesfully";
+}else{
+    die(mysqli_error($conect));
+}
 
-?>
-
-<?php
-   $host        = "127.0.0.1";
-   $port        = "5432";
-   $dbname      = "simple_crud";
-   $credentials = "avintha97";
-
-   $db = pg_connect( "$host $port $dbname $credentials"  );
-   if(!$db) {
-      echo "Error : Unable to open database\n";
-   } else {
-      echo "Opened database successfully\n";
-   }
 ?>
