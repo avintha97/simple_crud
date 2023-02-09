912d939970c4c8c64910cab05cf819894e767a08
avintha97 <avintha.geo@gmail.com>
2023-02-09 17:14:33 +0530

    data not visible inside the table

--- connect.php
+++ connect.php
@@ -1,9 +1,23 @@
 <?php
 
-$con = new mysqli("localhost","root","avintha97",'simple_crud');
+// $con = new mysqli("localhost","root","avintha97",'simple_crud');
 
-if(!$con){
-    die(mysqli_error($con));
-}
+// if(!$con){
+//     die(mysqli_error($con));
+// }
 
+?>
+
+<?php
+   $host        = "127.0.0.1";
+   $port        = "5432";
+   $dbname      = "simple_crud";
+   $credentials = "avintha97";
+
+   $db = pg_connect( "$host $port $dbname $credentials"  );
+   if(!$db) {
+      echo "Error : Unable to open database\n";
+   } else {
+      echo "Opened database successfully\n";
+   }
 ?>
--- insert.php
+++ insert.php
@@ -2,7 +2,13 @@
 
 include 'connect.php';
 
+extract($_POST);
 
+if(isset($_POST['Sname']) && isset($_POST['Email']) && isset($_POST['Stel'])  && isset($_POST['Saddress'])){
+
+    $sql = 'INSERT INTO  user_inormation (name,email,telephone,address) VALUES ("$Sname","$Email","$Stel","$Saddress")';
+    $result=pg_query($con,$sql);
+}
 
 
 
